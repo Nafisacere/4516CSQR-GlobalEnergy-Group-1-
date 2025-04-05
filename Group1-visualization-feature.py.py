d53ea5f4bb61900f9b5437f6ff747dec09fc5dc8
Nafisa <129067970+Nafisacere@users.noreply.github.com>
2025-04-05 20:30:21 +0300

    Made Comments

--- Group1-visualization-feature.py.py
+++ Group1-visualization-feature.py.py
@@ -1,20 +1,21 @@
-
+# Importing libraries needed for data handling, plotting, and the GUI
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import ttk
 
-
+# Load the cleaned energy usage data from a CSV file
 file_path = "Cleaned_Renewable_Energy_Usage_Sampled.csv"
 data = pd.read_csv(file_path)
 
-
+# Group the data by energy source and sum up the cost savings for each
 aggregated_data = data.groupby("Energy_Source")["Cost_Savings_USD"].sum()
 
-
+# Define custom colors for the pie chart sections
 colors = ['#ff9999', '#66b3ff', '#99ff99', '#ffcc99', '#c2c2f0', '#ffb3e6']
 
+#To create and display the pie chart when button is clicked
 def show_pie_chart():
     plt.figure(figsize=(10, 8)) 
     
@@ -27,16 +28,17 @@ def show_pie_chart():
         textprops={'fontsize': 12}, 
         wedgeprops={'edgecolor': 'black'}  
     )
-
-
+    
+  # Add a title to the chart
     plt.title("Proportion of Total Cost Savings by Energy Source", fontsize=16, fontweight='bold')
 
-
+  # Add a legend to the side with energy source labels
     plt.legend(wedges, aggregated_data.index, title="Energy Sources", loc="center left", bbox_to_anchor=(1, 0.5), fontsize=12)
 
-
+# Adjust layout to make sure everything fits
     plt.tight_layout()
-
+    
+# # Display the pie chart
     plt.show()
 
 
